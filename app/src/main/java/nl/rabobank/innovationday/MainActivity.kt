ca5b5383b97f8a84be691ca69d940130232c022d
Manuel Chris-Ogar <manuel.chris-ogar@rabobank.nl>
2026-02-03 13:24:23 +0100

    test pr review

--- app/src/main/java/nl/rabobank/innovationday/MainActivity.kt
+++ app/src/main/java/nl/rabobank/innovationday/MainActivity.kt
@@ -16,7 +16,7 @@ import nl.rabobank.innovationday.ui.theme.InnovationDayTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // simple comment
+        // simple comment2
         enableEdgeToEdge()
         setContent {
             InnovationDayTheme {
