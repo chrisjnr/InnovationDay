7366543f0c955e870f1995a1025e3829ed563e26
Manuel Chris-Ogar <manuel.chris-ogar@rabobank.nl>
2026-02-03 21:23:35 +0100

    changes to be checked

--- app/src/main/java/nl/rabobank/innovationday/MainActivity.kt
+++ app/src/main/java/nl/rabobank/innovationday/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // simple comment
+        // another one
         // TODO:  has to be
         enableEdgeToEdge()
         setContent {
