e87fdfd52827ca27917c32ed58aefa4ba43fcc75
Manuel Chris-Ogar <manuel.chris-ogar@rabobank.nl>
2026-02-03 14:46:55 +0100

    added todo

--- app/src/main/java/nl/rabobank/innovationday/MainActivity.kt
+++ app/src/main/java/nl/rabobank/innovationday/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import nl.rabobank.innovationday.ui.theme.InnovationDayTheme
 
 class MainActivity : ComponentActivity() {
+    // todo:  clean uo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // simple comment
